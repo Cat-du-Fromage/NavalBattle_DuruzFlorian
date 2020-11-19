5f6422291e421f12589fea2ddb68f977ef733e2a
Cat-du-Fromage <florian.duruz@gmail.com>
2020-11-19 16:02:48 +0100

    Premier commit
    Liaison du projet à github

---
+++ ProjetBato/cmake-build-debug/CMakeFiles/ProjetBato.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetBato.dir/main.c.obj"
+  "ProjetBato.exe"
+  "ProjetBato.exe.manifest"
+  "ProjetBato.pdb"
+  "libProjetBato.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProjetBato.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
